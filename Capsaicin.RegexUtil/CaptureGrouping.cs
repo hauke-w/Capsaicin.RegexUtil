11abda3d37fb8e6a29c9f8d869dd8a0832318ea1
Hauke Wittern <hauke.wittern@minebea-intec.com>
2021-01-20 20:57:11 +0100

    changed nullability

--- Capsaicin.RegexUtil/CaptureGrouping.cs
+++ Capsaicin.RegexUtil/CaptureGrouping.cs
@@ -7,32 +7,32 @@ namespace Capsaicin.RegexUtil
 {
     public interface ICaptureGrouping1 : ICaptureGrouping
     {
-        IEnumerable<string?> Values
-            => Captures.Select(c => c[0]?.Value);
+        IEnumerable<string> Values
+            => Captures.Select(c => c[0]!.Value);
     }
 
     public interface ICaptureGrouping2 : ICaptureGrouping1
     {
-        new IEnumerable<(string? Value1, string? Value2)> Values
-            => Captures.Select(c => (c[0]?.Value, c[1]?.Value));
+        new IEnumerable<(string Value1, string? Value2)> Values
+            => Captures.Select(c => (c[0]!.Value, c[1]?.Value));
     }
 
     public interface ICaptureGrouping3 : ICaptureGrouping2
     {
-        new IEnumerable<(string? Value1, string? Value2, string? Value3)> Values
-            => Captures.Select(c => (c[0]?.Value, c[1]?.Value, c[2]?.Value));
+        new IEnumerable<(string Value1, string? Value2, string? Value3)> Values
+            => Captures.Select(c => (c[0]!.Value, c[1]?.Value, c[2]?.Value));
     }
 
     public interface ICaptureGrouping4 : ICaptureGrouping3
     {
-        new IEnumerable<(string? Value1, string? Value2, string? Value3, string? Value4)> Values
-            => Captures.Select(c => (c[0]?.Value, c[1]?.Value, c[2]?.Value, c[3]?.Value));
+        new IEnumerable<(string Value1, string? Value2, string? Value3, string? Value4)> Values
+            => Captures.Select(c => (c[0]!.Value, c[1]?.Value, c[2]?.Value, c[3]?.Value));
     }
 
     public interface ICaptureGrouping5 : ICaptureGrouping4
     {
-        new IEnumerable<(string? Value1, string? Value2, string? Value3, string? Value4, string? Value5)> Values
-            => Captures.Select(c => (c[0]?.Value, c[1]?.Value, c[2]?.Value, c[3]?.Value, c[4]?.Value));
+        new IEnumerable<(string Value1, string? Value2, string? Value3, string? Value4, string? Value5)> Values
+            => Captures.Select(c => (c[0]!.Value, c[1]?.Value, c[2]?.Value, c[3]?.Value, c[4]?.Value));
     }
 
     public class CaptureGrouping : ICaptureGrouping, ICaptureGrouping5
