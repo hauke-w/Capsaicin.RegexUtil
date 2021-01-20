8f265d646638c9708e21db2949a83ccc24eb0dc1
Hauke Wittern <hauke.wittern@minebea-intec.com>
2021-01-20 12:23:31 +0100

    added test

--- Capsaicin.RegexUtil.Tests/CaptureGroupTest.cs
+++ Capsaicin.RegexUtil.Tests/CaptureGroupTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace Capsaicin.RegexUtil
 {
@@ -39,10 +40,58 @@ namespace Capsaicin.RegexUtil
                 var actual3 = testObject2.First("id");
 
                 var (idIndex, nameIndex, valueIndex) = expected[i];
-                Assert.AreEqual(idCaptures[idIndex], actual3);
-                Assert.AreEqual(nameCaptures[nameIndex], actual1);
-                Assert.AreEqual(valueCaptures[valueIndex], actual2);
+                Assert.AreSame(idCaptures[idIndex], actual3);
+                Assert.AreSame(nameCaptures[nameIndex], actual1);
+                Assert.AreSame(valueCaptures[valueIndex], actual2);
                 i++;
+            }
+        }
+
+        [TestMethod]
+        [DataRow("@PROP=@NAME=URI@ENDNAME;@VALU=myValue@ENDVALU;@PRMT=@ENDPRMT;@ENDPROP;", "NAME,URI|VALU,myValue|PRMT,")]
+        [DataRow("@PROP=@NAME=URI@ENDNAME;@TYPE=@ENDTYPE;@VALU=myValue@ENDVALU;@PRMT=@ENDPRMT;@ENDPROP;", "NAME,URI|TYPE,|VALU,myValue|PRMT,")]
+        public void SelectTest(string expression, string expectedCaptureIndexesExpression)
+        {
+            var regex = new Regex(@"^(?<prop>@PROP=(?<kvp>@(?<name>\w+)=(?<value>([^@]|@(?!\k<name>))+)?@END\k<name>;)+@ENDPROP;)*$");
+            var match = regex.Match(expression);
+            Assert.IsTrue(match.Success, "Invalid test data");
+
+            var testObjects = match.Group("name", "value")
+                .By("prop")
+                .ToList();
+
+            var expectedData = expectedCaptureIndexesExpression
+                .Split(';')
+                .Select(prop => prop
+                    .Split('|')
+                    .Select(kvp =>
+                    {
+                        var fragments = kvp.Split(',');
+                        return (Name: fragments[0],
+                                Value: string.IsNullOrEmpty(fragments[1]) ? null : fragments[1]);
+                    }).ToList())
+                .ToList();
+
+            for (var i = 0; i < testObjects.Count; i++)
+            {
+                var testObject = testObjects[i];
+                var actual = testObject.Select("name", "value");
+                Assert.IsNotNull(actual);
+                var actualAsList = actual.ToList();
+
+                var expectedPropertyData = expectedData[i];
+                Assert.AreEqual(expectedPropertyData.Count, actualAsList.Count);
+
+                for (int j = 0; j < expectedPropertyData.Count; j++)
+                {
+                    var actualKvp = actualAsList[j];
+                    Assert.AreEqual(2, actualKvp.Length);          
+                    var actualName = actualKvp[0];
+                    var actualValue = actualKvp[1];
+                    var (expectedName, expectedValue) = expectedPropertyData[j];
+                    Assert.AreEqual(expectedName, actualName);
+                    Assert.AreEqual(expectedValue, actualValue);
+                }                
             }
         }
     }
